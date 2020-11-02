d2d289fcab2ea20dc91e61eec75f1a7f90073d81
Lisa <yanique.dickson@gmail.com>
2020-11-02 14:06:42 -0500

    Updated alignment in Git_Intro

--- Git_Intro.js
+++ Git_Intro.js
@@ -2,10 +2,15 @@ var house = "This is my house"
 var newHouse = function() {
 a = "This is an awesome house"
 };
+
 var nextHouse = function() {
     a = "This is another house"
-    };
+};
 
-    var lisaHouse = function() {
-        b = "This is Lisa's house"
-        };
+var lisaHouse = function() {
+    b = "This is Lisa's house"
+};
+
+var lisaTestFour = function() {
+    b = "This is Lisa's house"
+};
